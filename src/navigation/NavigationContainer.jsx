e3100968810279fee27d1e13aea66e70339a7879
Tiana-K08 <tetkononenko2020@gmail.com>
2025-11-25 14:42:13 +0200

    feat: add navigation styles

--- src/navigation/NavigationContainer.jsx
+++ src/navigation/NavigationContainer.jsx
@@ -2,12 +2,23 @@ import { NavLink } from 'react-router-dom';
 
 export default function NavigationContainer() {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/blog">Blog</NavLink>
-      {/* {false ? <button>Add Blog</button> : null} */}
+    <div className="nav-container">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink to="/">Home</NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/about">About</NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog">Blog</NavLink>
+        </div>
+        {/* {false ? <button>Add Blog</button> : null} */}
+      </div>
+      <div className="right-side">Tetiana Kononenko</div>
     </div>
   );
 }
