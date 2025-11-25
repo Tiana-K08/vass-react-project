f039c15b7942a0ffc4b764a16f1b5a930c01c10d
Tiana-K08 <tetkononenko2020@gmail.com>
2025-11-25 11:49:38 +0200

    feat: extend HomepageItem with images

--- src/components/homepage/HomepageItem.jsx
+++ src/components/homepage/HomepageItem.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 
 export default function HomepageItem(props) {
-  // thumb_image_url, logo_url, description
-  const { id, name, url } = props.item;
+  const { id, name, url, thumb_image_url, logo_url, description } = props.item;
 
   return (
     <>
+      <img src={thumb_image_url} alt="bg-image" />
+      <img src={logo_url} alt="bg-image" />
       <h3>{name}</h3>
       <p>{url}</p>
+      <p>{description}</p>
       <Link to={`/home/${id}`}>More details about {id}</Link>
     </>
   );
